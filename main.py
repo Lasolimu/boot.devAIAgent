37b96aebc1118a6af94ea1067075307cc646c5ae
Lasolimu <Lasolimu@gmail.com>
2026-03-27 09:48:36 -0600

    Track tokens for prompt and response.

--- main.py
+++ main.py
@@ -13,11 +13,8 @@ model = "gemini-2.5-flash"
 content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
 response = client.models.generate_content(model=model, contents=content)
+if response.usage_metadata == None:
+    raise RuntimeError("api request failed")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
-
-def main():
-    print("Hello from aiagent!")
-
-
-if __name__ == "__main__":
-    main()
