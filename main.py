126a99a2017170e277423ff9e63c0de19f86268d
Lasolimu <Lasolimu@gmail.com>
2026-03-27 10:06:55 -0600

    Create a tag to allow verbose output showing the prompt and token usage along with the response.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os, argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 parser = argparse.ArgumentParser(description="Google Gemini")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 load_dotenv()
@@ -15,10 +17,13 @@ client = genai.Client(api_key=apiKey)
 
 model = "gemini-2.5-flash"
 content = args.user_prompt
+messages = [types.Content(role="user", parts=[types.Part(text=content)])]
 
-response = client.models.generate_content(model=model, contents=content)
+response = client.models.generate_content(model=model, contents=messages)
 if response.usage_metadata == None:
     raise RuntimeError("api request failed")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {content}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(response.text)
